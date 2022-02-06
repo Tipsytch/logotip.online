3f2b46a622cacbc579555b6f1a7c0af54f48c4f9
Tipsytch <91761137+Tipsytch@users.noreply.github.com>
2022-02-06 18:35:38 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function(){
+$('.icon-menu').click(function(event) {
+    $(this).toggleClass('active');
+    $('.menu__icon, .menu__body').toggleClass('active');   
+    if($(this).hasClass('active')){
+        $('body').data('scroll',$(window).scrollTop());
+    }
+    $('body').toggleClass('lock');
+    if(!$(this).hasClass('active')){
+        $('body,html').scrollTop(parseInt($('body').data('scroll')));
+    }
+});
+
+function ibg() {
+    $.each($('.ibg'), function(index, val){
+        if($(this).find('img').length>0){
+            $(this).css('background-image','url("'+$(this).find('img').attr('src')+'")');
+        }
+    });
+}
+ibg();
+console.log('done');     	
+});
